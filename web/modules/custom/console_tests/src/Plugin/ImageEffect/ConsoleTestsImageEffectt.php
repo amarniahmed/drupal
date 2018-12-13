dc4fc5a05ce51cedec66b2fc06fb4e854eca4147
ahmed <ahmed.amarni1985@gamil.com>
2018-12-13 11:11:03 +0100

    ajout du fichier sur le module console tests

---
+++ web/modules/custom/console_tests/src/Plugin/ImageEffect/ConsoleTestsImageEffectt.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\console_tests\Plugin\ImageEffect;
+
+use Drupal\Core\Image\ImageInterface;
+use Drupal\image\ImageEffectBase;
+
+/**
+ * Provides a 'ConsoleTestsImageEffectt' image effect.
+ *
+ * @ImageEffect(
+ *  id = "console_tests_image_effectt",
+ *  label = @Translation("Console tests image effectt"),
+ *  description = @Translation("console tests Image blur")
+ * )
+ */
+class ConsoleTestsImageEffectt extends ImageEffectBase {
+
+  /**
+   * {@inheritdoc}
+   */
+  public function applyEffect(ImageInterface $image) {
+    // Implement Image Effect.
+    return imagefilter($image->getToolkit()->getResource());
+  }
+
+}
