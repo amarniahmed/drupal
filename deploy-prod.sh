8e3110e953b7d6707b389b1c6a83a71efaf29e20
ahmed <ahmed.amarni1985@gamil.com>
2019-01-03 11:46:24 +0100

    Suppression du fichier form.settings.yml

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,10 +1,29 @@
 #!/usr/bin/env bash
+
+drush sset system.maintennance_mode 1
+
+echo Maintenance mode enabled.
+
+drush cr
+
 git pull origin master
 
 composer install --no-dev
+
+drush cr
 
 drush updb
 
 drush cr
 
-drush csex prod
+drush csex prod -y
+
+drush cim -y
+
+drush cr
+
+drush sset system.maintennance_mode 0
+
+drush cr
+
+echo Site is online.
